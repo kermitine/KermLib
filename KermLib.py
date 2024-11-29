b3abf69567f77f0616137fa65842d9679ad8d284
Ayrik Nabirahni <ayrytube@gmail.com>
2024-11-28 19:24:02 -0800

    added comments

--- KermLib.py
+++ KermLib.py
@@ -11,21 +11,21 @@ class KermLib():
     def __init__(self, version):
         self.version = version
 
-    def text_extraction(self, complex_string): # removes special characters and converts sentence to simple lowercase text
+    def text_extraction(self, complex_string):  # removes special characters and converts sentence to simple lowercase text
         new_string_list = []
         new_string_list_words_joined = []
         complex_string = complex_string.lower()
         complex_string_list = complex_string.split()
-        for word in complex_string_list:
+        for word in complex_string_list:    # creates a list of sub-lists, each sub-list contains each char for each word
             new_string = [char for char in word if char in alphabet]
             new_string_list.append(new_string)
-        print(new_string_list)
-        for sub_list in new_string_list:
+        for sub_list in new_string_list:    # joins each sub-list
             joined_sub_list = ''.join(sub_list)
             new_string_list_words_joined.append(joined_sub_list)
-        simplified_string = ' '.join(new_string_list_words_joined)
+        simplified_string = ' '.join(new_string_list_words_joined)  # joins each list back into a string
         return simplified_string
 
 
-
 KermLib = KermLib('2024.11.28.1915.alpha')
+
+print(KermLib.version)
