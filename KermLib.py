f9e3726fe302963ec1ec1745e06b192135a0babd
Ayrik Nabirahni <ayrytube@gmail.com>
2024-11-28 18:36:00 -0800

    made class and object, version added

--- KermLib.py
+++ KermLib.py
@@ -1,1 +1,10 @@
 # personal library that features word/sentence processing, and more
+
+
+class KermLib():
+    def __init__(self, version):
+        self.version = version
+
+
+
+KermLib = KermLib('2024.11.28.1835.alpha')
